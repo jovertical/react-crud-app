58f9eac02d3894f0f0ac7d8e4103df64bf1219e1
Jovert Palonpon <palonponjovertlota@gmail.com>
2020-02-21 18:04:25 +0800

    feath: Create a user

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import * as React from 'react'
 import Box from '@material-ui/core/Box'
+import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
 import fetch from 'unfetch'
+import FormDialog from './components/Dialogs/Form'
 import Header from './components/Header'
 import Snackbar from './components/Snackbar'
 import Table from './components/Table'
 
 const API_URL = 'https://reqres.in/api'
+
+const defaultHeaders = {
+  'Content-Type': 'application/json',
+}
 
 const properties = {
   name: {
@@ -22,11 +28,48 @@ const properties = {
 export default function App() {
   const [users, setUsers] = React.useState([])
   const [message, setMessage] = React.useState(null)
+  const [creating, setCreating] = React.useState(false)
+
+  const handleCreate = async fields => {
+    try {
+      const res = await fetch(API_URL + '/users', {
+        method: 'POST',
+        headers: defaultHeaders,
+        body: JSON.stringify(fields),
+      })
+
+      if (res.status === 201) {
+        const data = await res.json()
+        const newUser = {
+          id: data.id,
+          first_name: data.firstName,
+          last_name: data.lastName,
+          email: data.email,
+          created_at: data.createdAt,
+        }
+        setUsers([newUser].concat(users.splice(0, users.length - 1)))
+
+        setCreating(false)
+        setMessage({
+          message: 'User successfuly created!',
+          severity: 'success',
+        })
+      } else {
+        throw new Error('Cannot create User!')
+      }
+    } catch (error) {
+      setMessage({
+        message: error.message,
+        severity: 'error',
+      })
+    }
+  }
 
   const handleDelete = async row => {
     try {
       const res = await fetch(`${API_URL}/users/${row.id}`, {
         method: 'DELETE',
+        headers: defaultHeaders,
       })
 
       if (res.status === 204) {
@@ -39,6 +82,7 @@ export default function App() {
       }
     } catch (error) {
       setMessage({
+        message: error.message,
         severity: 'error',
       })
     }
@@ -49,12 +93,7 @@ export default function App() {
 
     if (res.status === 200) {
       const collection = await res.json()
-      const data = collection.data.map(user => ({
-        name: `${user.first_name} ${user.last_name}`,
-        ...user,
-      }))
-
-      setUsers(data)
+      setUsers(collection.data)
     } else {
       setMessage({
         message: 'Cannot fetch users, please try again.',
@@ -84,9 +123,40 @@ export default function App() {
       <Header />
       <Box mb={5} />
       <Container maxWidth="md">
-        <Table properties={properties} data={users} onDelete={handleDelete} />
+        <Box display="flex" justifyContent="flex-end">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => setCreating(true)}
+          >
+            Create
+          </Button>
+        </Box>
+      </Container>
+      <Box mb={2} />
+      <Container maxWidth="md">
+        <Table
+          properties={properties}
+          data={users.map(user => ({
+            ...user,
+            name: `${user.first_name || ''} ${user.last_name || ''}`,
+          }))}
+          onDelete={handleDelete}
+        />
       </Container>
       {message && <Snackbar open {...message} />}
+      {creating && (
+        <FormDialog
+          open
+          onSave={handleCreate}
+          defaultValues={{
+            firstName: '',
+            lastName: '',
+            email: '',
+          }}
+          onClose={() => setCreating(false)}
+        />
+      )}
     </React.Fragment>
   )
 }
---
+++ src/components/Dialogs/Form.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { makeStyles } from '@material-ui/core/styles'
+import Box from '@material-ui/core/Box'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogTitle from '@material-ui/core/DialogTitle'
+
+const useStyles = makeStyles(theme => ({
+  dialog: {
+    width: 500,
+  },
+}))
+
+export default function Form({ defaultValues, onClose, onSave, ...props }) {
+  const styles = useStyles()
+  const [open, setOpen] = React.useState(props.open || false)
+  const [firstName, setFirstName] = React.useState(defaultValues.firstName)
+  const [lastName, setLastName] = React.useState(defaultValues.lastName)
+  const [email, setEmail] = React.useState(defaultValues.email)
+
+  const handleClose = () => {
+    setOpen(false)
+    onClose()
+  }
+
+  return (
+    <div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">Create a New User</DialogTitle>
+        <DialogContent className={styles.dialog}>
+          <Box mb={1}>
+            <TextField
+              autoFocus
+              margin="dense"
+              fullWidth
+              label="First name"
+              type="text"
+              value={firstName}
+              onChange={event => setFirstName(event.target.value)}
+            />
+          </Box>
+          <Box mb={1}>
+            <TextField
+              autoFocus
+              margin="dense"
+              fullWidth
+              label="Last name"
+              type="text"
+              value={lastName}
+              onChange={event => setLastName(event.target.value)}
+            />
+          </Box>
+          <Box mb={1}>
+            <TextField
+              autoFocus
+              margin="dense"
+              fullWidth
+              label="Email Address"
+              type="email"
+              value={email}
+              onChange={event => setEmail(event.target.value)}
+            />
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button
+            onClick={() => onSave && onSave({ firstName, lastName, email })}
+            color="primary"
+          >
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  )
+}
+
+Form.propTypes = {
+  open: PropTypes.bool,
+  defaultValues: PropTypes.shape({
+    firstName: PropTypes.string,
+    lastName: PropTypes.string,
+    email: PropTypes.string,
+  }),
+  onClose: PropTypes.func,
+  onSave: PropTypes.func,
+}
